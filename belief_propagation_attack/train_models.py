83a846e53b51e01c4e7164d855f1e2a81dab342f
TMarquet <72377479+TMarquet@users.noreply.github.com>
2020-10-29 17:46:20 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -250,8 +250,9 @@ def cnn_aes_hd(input_length=700, learning_rate=0.00001, classes=256, dense_units
         # Create model.
         model = Model(inputs, x, name='cnn_best')
         optimizer = RMSprop(lr=learning_rate)
-        model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    return model
+        parallel_model = keras.utils.multi_gpu_model(model, NUM_GPU)
+        parallel_model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return parallel_model
 
 ### CNN Best model
 def cnn_best(input_length=700, learning_rate=0.00001, classes=256, dense_units=4096):
