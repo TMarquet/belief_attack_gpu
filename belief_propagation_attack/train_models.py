1046272e499c30ca9223dc86a76d374eecca0314
TMarquet <72377479+TMarquet@users.noreply.github.com>
2020-11-17 17:47:36 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -573,7 +573,7 @@ if __name__ == "__main__":
     if ALL_VARS:
         variable_list = get_variable_list()
     elif ALL_VARIABLE is None:
-        variable_list = ['s003','s004','s005','s006','s007','s008']
+        variable_list = ['cm001','s003','s004','s005','s006','s007','s008']
         print variable_list
     else:
         variable_list = ['{}{}'.format(ALL_VARIABLE, pad_string_zeros(i+1)) for i in range(variable_dict[ALL_VARIABLE])]
