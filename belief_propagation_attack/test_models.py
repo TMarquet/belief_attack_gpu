dcd78ffbb0f5f35b7c25469ec44b5afca20153d4
TMarquet <72377479+TMarquet@users.noreply.github.com>
2020-11-18 16:04:57 +0100

    tset

--- belief_propagation_attack/test_models.py
+++ belief_propagation_attack/test_models.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib
 import os
 matplotlib.use('Agg')
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
 import matplotlib.pyplot as plt
 import argparse
 from utility import *
