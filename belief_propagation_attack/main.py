4a1909ffd4eeafa069014a8a9dde1e048205093f
TMarquet <72377479+TMarquet@users.noreply.github.com>
2020-11-28 15:54:51 +0100

    test

--- belief_propagation_attack/main.py
+++ belief_propagation_attack/main.py
@@ -8,7 +8,7 @@ import cPickle as Pickle
 from datetime import datetime
 import timing
 import matplotlib.pyplot as plt
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 CHOSEN_KEY = [0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79, 0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20, 0x46, 0x75]
 
 def run_belief_propagation_attack(margdist=None):
