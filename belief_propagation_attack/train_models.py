13ddbc22a3ad09b16569ca580e6c71225ca261d6
TMarquet <72377479+TMarquet@users.noreply.github.com>
2020-12-13 16:00:32 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -394,10 +394,11 @@ def train_model(X_profiling, Y_profiling, model, save_file_name, epochs=150, bat
         reshaped_y = hamming_distance_encode_bulk(Y_profiling)
         reshaped_val = hamming_distance_encode_bulk(validation_data[1])
     elif one_hot:
-        reshaped_y = to_categorical(Y_profiling, num_classes=9 if hammingweight else 256)
-        reshaped_val = to_categorical(validation_data[1], num_classes=9 if hammingweight else 256)
         print(reshaped_y.shape)
         print(reshaped_val.shape)
+        reshaped_y = to_categorical(Y_profiling, num_classes=9 if hammingweight else 256)
+        reshaped_val = to_categorical(validation_data[1], num_classes=9 if hammingweight else 256)
+
     else:
         reshaped_y = Y_profiling
         reshaped_val = validation_data[1]
