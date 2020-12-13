114dcee5937f32f78b8a6677f24d2ab2c367625d
TMarquet <72377479+TMarquet@users.noreply.github.com>
2020-12-13 23:08:02 +0100

    tset

--- belief_propagation_attack/test_models.py
+++ belief_propagation_attack/test_models.py
@@ -417,7 +417,7 @@ if __name__ == "__main__":
     # print "*** TEST VARIABLE {} ***".format(VARIABLE)
     data = np.array([])
     for file in listdir('output/cm'):
-        data.append(np.loadtxt(file))
+        np.append(data,np.loadtxt(file))
     print(data.shape)
     model_tester = TestModels(jitter=JITTER, use_extra=(not RANDOM_KEY) and USE_EXTRA, no_print=not DEBUG, verbose=VERBOSE, histogram=HISTOGRAM)
 
