ebd15f211a36c7bb4336e1ceaf7742bed5e4424a
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-01-13 18:04:21 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -679,7 +679,7 @@ if __name__ == "__main__":
                                                                       load_metadata=LOAD_METADATA)
         else:
             
-            ASCAD_data_folder = "/root/Projets/ASCAD/ATMEGA_AES_v1/ATM_AES_v1_variable_key/ASCAD_data/ASCAD_databases"
+            ASCAD_data_folder = "/root/Projets/ASCAD/ATMEGA_AES_v1/ATM_AES_v1_variable_key/ASCAD_data/ASCAD_databases/"
             
             # Choose the name of the model
 
@@ -697,9 +697,9 @@ if __name__ == "__main__":
             #Traces Scaling (between 0 and 1)
             scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
             X_profiling = scaler.fit_transform(X_profiling)
-            X_attack = scaler.transform(X_attack)
+            # X_attack = scaler.transform(X_attack)
             
-            X_attack = X_attack.reshape((X_attack.shape[0], X_attack.shape[1], 1))            
+            # X_attack = X_attack.reshape((X_attack.shape[0], X_attack.shape[1], 1))            
             
             
             
@@ -709,7 +709,7 @@ if __name__ == "__main__":
             print "|| Changing Input Length from {} to {} (max samples)".format(INPUT_LENGTH, X_profiling.shape[1])
             INPUT_LENGTH = X_profiling.shape[1]
 
-        train_variable_model(variable, X_profiling, Y_profiling, X_attack, Y_attack, mlp=USE_MLP, cnn=USE_CNN, cnn_pre=USE_CNN_PRETRAINED, lstm=USE_LSTM, input_length=INPUT_LENGTH, add_noise=ADD_NOISE, epochs=EPOCHS,
+        train_variable_model(variable, X_profiling[:190000], Y_profiling[:190000], X_Profiling[190000:], Y_Profiling[190000:], mlp=USE_MLP, cnn=USE_CNN, cnn_pre=USE_CNN_PRETRAINED, lstm=USE_LSTM, input_length=INPUT_LENGTH, add_noise=ADD_NOISE, epochs=EPOCHS,
             training_traces=TRAINING_TRACES, mlp_layers=MLP_LAYERS, mlp_nodes=MLP_NODES, lstm_layers=LSTM_LAYERS, lstm_nodes=LSTM_NODES, batch_size=BATCH_SIZE, sd=STANDARD_DEVIATION, augment_method=AUGMENT_METHOD, jitter=JITTER, progress_bar=PROGRESS_BAR,
             learning_rate=LEARNING_RATE,weight_init=weight_method_test, multilabel=MULTILABEL, hammingweight=HAMMINGWEIGHT, loss_function=LOSS_FUNCTION, hamming_distance_encoding=HAMMING_DISTANCE_ENCODING, scratch_storage=SCRATCH_STORAGE, use_ascad=USE_ASCAD)
 
