9c8cd332bbb45fa53e1fbcabb7e7defb98dfe09f
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-01-13 20:40:02 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -759,11 +759,19 @@ if __name__ == "__main__":
             
             
         # Handle Input Length of -1
+        
         if INPUT_LENGTH < 0:
             # Set to length of X_profiling
             print "|| Changing Input Length from {} to {} (max samples)".format(INPUT_LENGTH, X_profiling.shape[1])
             INPUT_LENGTH = X_profiling.shape[1]
-
+        
+        
+        for elem in X_profiling:
+            middle = INPUT_LENGTH/2
+            elem = elem[int(midlle*0.5):int(middle*1.5)]
+        for elem in X_validation:
+            middle = INPUT_LENGTH/2
+            elem = elem[int(midlle*0.5):int(middle*1.5)]        
         train_variable_model(variable, X_profiling, Y_profiling, X_validation, Y_validation, mlp=USE_MLP, cnn=USE_CNN, cnn_pre=USE_CNN_PRETRAINED, lstm=USE_LSTM, input_length=INPUT_LENGTH, add_noise=ADD_NOISE, epochs=EPOCHS,
             training_traces=TRAINING_TRACES, mlp_layers=MLP_LAYERS, mlp_nodes=MLP_NODES, lstm_layers=LSTM_LAYERS, lstm_nodes=LSTM_NODES, batch_size=BATCH_SIZE, sd=STANDARD_DEVIATION, augment_method=AUGMENT_METHOD, jitter=JITTER, progress_bar=PROGRESS_BAR,
             learning_rate=LEARNING_RATE,weight_init=weight_method_test, multilabel=MULTILABEL, hammingweight=HAMMINGWEIGHT, loss_function=LOSS_FUNCTION, hamming_distance_encoding=HAMMING_DISTANCE_ENCODING, scratch_storage=SCRATCH_STORAGE, use_ascad=USE_ASCAD)
