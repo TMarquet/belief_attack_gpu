10f0d40ce3bc31947b34489aa9cbc61edd894267
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-01-13 18:28:31 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -656,7 +656,7 @@ if __name__ == "__main__":
         variable_list = get_variable_list()
     elif ALL_VARIABLE is None:
 
-        variable_list = ['xt001','xt004']
+        variable_list = ['s003']
         # for i in range(12,17) :
         #     if i < 10 :
         #         variable_list.append('mc00'+str(i))
