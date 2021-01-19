25dcaa51658af4c210883ede281f0826ee75370c
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-01-19 20:12:18 +0100

    test

--- belief_propagation_attack/realTraceHandler.py
+++ belief_propagation_attack/realTraceHandler.py
@@ -297,6 +297,7 @@ class RealTraceHandler:
         if not self.no_print:
             print "\n* Checking model {} (variable {}) {}*\n".format(model_name, variable, 'WITH VALIDATION TRACES' if from_end else '')
         if not check_file_exists(model_file):
+            print('Here')
             if not self.no_print:
                 print "!!! Doesn't exist!"
             return (None, None)
