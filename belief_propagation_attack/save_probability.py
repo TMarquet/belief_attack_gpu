3b13b2f9e1176f3530c22aa8700f8e1a5284f8a2
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-01-19 20:00:51 +0100

    test

---
+++ belief_propagation_attack/save_probability.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 19:59:53 2021
+
+@author: kahg8
+"""
+import sys
+import os
+import os.path
+
+from utility import *
+
+import timing
+
+from numpy import savetxt
+def save_probability_list(num_traces):
+    
+    for model_file in sorted(listdir(MODEL_FOLDER + 'adagrad/')):
+        var_name = get_variable_name(model_file)
+        var = var_name + get_variable_number(model_file)
+        print 'Saving probabilities for : ' + var
+        rank_list, prob_list, predicted_values = self.real_trace_handler.get_leakage_rank_list_with_specific_model(model_file, traces=num_traces,ASCAD= True)
+        if not get_variable_name(model_file) in listdir(OUTPUT_FOLDER):
+            os.mkdir(OUTPUT_FOLDER + var_name + '/')
+        savetxt(OUTPUT_FOLDER + var_name + '/' + var +'.csv', prob_list, delimiter=',')
