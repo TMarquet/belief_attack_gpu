0a00d141858946a75e69a319bb64072d3c169c62
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-01-25 13:10:35 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -29,6 +29,7 @@ from utility import *
 
 tf.random.set_seed(7)
 
+
 ###########################################################################
 
 class TrainValTensorBoard(TensorBoard):
@@ -637,7 +638,7 @@ if __name__ == "__main__":
         variable_list = get_variable_list()
     elif ALL_VARIABLE is None:
 
-        variable_list = ['s015','s016','k002','k003','k004','k005','k006']
+        variable_list = ['s002','s003','s004','s005','s006','s007','s008','s009','s010','s011','s012','s013','s014','k006','k007','k008']
         # for i in range(12,17) :
         #     if i < 10 :
         #         variable_list.append('mc00'+str(i))
