4a504fdd29325be07d6d68848ea2a832e4cd2f87
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-02-01 15:58:51 +0100

    test

--- belief_propagation_attack/save_probability.py
+++ belief_propagation_attack/save_probability.py
@@ -23,9 +23,10 @@ def save_probability_list(num_traces):
         var = var_name + str(get_variable_number(model_file))
         print 'Saving probabilities for : ' + var
         output_list = handler.get_leakage_rank_list_with_specific_model(MODEL_FOLDER +model_file, traces=num_traces,ASCAD= True,save_proba = True)
+        print(output_list.shape)
         if not var_name in listdir(OUTPUT_FOLDER):
             os.mkdir(OUTPUT_FOLDER + var_name + '/')
         savetxt(OUTPUT_FOLDER + var_name + '/' + var +'.csv', output_list, delimiter=',')
         
 
-save_probability_list(1)
+save_probability_list(2)
