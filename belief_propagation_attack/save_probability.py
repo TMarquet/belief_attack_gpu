00151de41f89201cb2fa8680d500209bfb7c5e4c
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-02-01 23:38:54 +0100

    test

--- belief_propagation_attack/save_probability.py
+++ belief_propagation_attack/save_probability.py
@@ -16,7 +16,7 @@ from numpy import savetxt
 
 
 ##### ATTENTION #############
-# cm 5 ->8 a refaire (pas♣ de x10 dans l'optimizer sans doute) mc 7 8
+# cm 5 ->8 a refaire (pas♣ de x10 dans l'optimizer sans doute) mc 7 8 p 31 32
 
 def save_probability_list(num_traces):
     model_tester = TestModels(use_extra = False)
@@ -26,13 +26,13 @@ def save_probability_list(num_traces):
         
         print(model_file)
         var_name = get_variable_name(model_file)
-        if var_name == 'cm' or var_name == 'h' or var_name == 'k' or var_name == 'mc' or var_name == 'p' or var_name == 's':
+        if var_name == 'cm' or var_name == 'h' or var_name == 'k' or var_name == 'mc' or var_name == 'p' or var_name == 's' or var_name == 't':
             continue
         var = var_name + str(get_variable_number(model_file))
         out_list = []
         for i in range( 0,8):
             out_list.append(i)
-        if var_name == 't' and get_variable_number(model_file) in out_list:
+        if var_name == 'xt' and get_variable_number(model_file) in out_list:
             continue
         print 'Saving probabilities for : ' + var
         output_list , prob_list , rank_list = handler.get_leakage_rank_list_with_specific_model(MODEL_FOLDER +model_file, traces=num_traces,ASCAD= False,save_proba = True)
