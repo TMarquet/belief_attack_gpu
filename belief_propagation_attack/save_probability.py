c2ae3c52836dce1e2e40a25056a26772b0310246
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-02-02 09:03:29 +0100

    test

--- belief_propagation_attack/save_probability.py
+++ belief_propagation_attack/save_probability.py
@@ -23,17 +23,14 @@ def save_probability_list(num_traces):
     handler = model_tester.real_trace_handler
 
     for model_file in sorted(listdir(MODEL_FOLDER)):
-        
+        out_list = ['cm5','cm6','cm7','cm8','mc7','mc8','p31','p32']
+      
         print(model_file)
         var_name = get_variable_name(model_file)
-        if var_name == 'cm' or var_name == 'h' or var_name == 'k' or var_name == 'mc' or var_name == 'p' or var_name == 's' or var_name == 't':
-            continue
         var = var_name + str(get_variable_number(model_file))
-        out_list = []
-        for i in range( 0,8):
-            out_list.append(i)
-        if var_name == 'xt' and get_variable_number(model_file) in out_list:
+        if not var in out_list:
             continue
+
         print 'Saving probabilities for : ' + var
         output_list , prob_list , rank_list = handler.get_leakage_rank_list_with_specific_model(MODEL_FOLDER +model_file, traces=num_traces,ASCAD= False,save_proba = True)
         print "> Median Rank: {}".format(np.median(rank_list))
