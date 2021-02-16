46f95dc9512022a2039d0bd44e101b3372cc2dd6
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-02-16 16:03:23 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -267,36 +267,36 @@ def cnn_best(input_length=2000, learning_rate=0.00001, classes=256, dense_units=
     model.add(Lambda(lambda x: K.l2_normalize(x,axis=1)))
     model.add(BatchNormalization(name='block1_batchnorm'))
     model.add(tf.keras.layers.Activation('relu'))
-    model.add(MaxPooling1D(2, strides=2, name='block1_pool'))  
+  
     
     # Block 2
 
-    model.add(Conv1D(128, 3, padding='same', name='block2_conv1',kernel_initializer = weight_init_method))    
+    model.add(Conv1D(64, 3, padding='same', name='block2_conv1',kernel_initializer = weight_init_method))    
     model.add(Lambda(lambda x: K.l2_normalize(x,axis=1)))
     model.add(BatchNormalization(name='block2_batchnorm'))
     model.add(tf.keras.layers.Activation('relu'))
-    model.add(MaxPooling1D(2, strides=2, name='block2_pool'))  
+  
     
     # Block 3
-    model.add(Conv1D(256, 3, padding='same', name='block3_conv1',kernel_initializer = weight_init_method))
+    model.add(Conv1D(128, 3, padding='same', name='block3_conv1',kernel_initializer = weight_init_method))
     model.add(Lambda(lambda x: K.l2_normalize(x,axis=1)))
     model.add(BatchNormalization(name='block3_batchnorm'))
     model.add(tf.keras.layers.Activation('relu'))
-    model.add(MaxPooling1D(2, strides=2, name='block3_pool'))
+    model.add(AveragePooling1D(2, strides=2, name='block3_pool'))
     
     # Block 4
     model.add(Conv1D(512, 3, padding='same', name='block4_conv1',kernel_initializer = weight_init_method))
     model.add(Lambda(lambda x: K.l2_normalize(x,axis=1)))
     model.add(BatchNormalization(name='block4_batchnorm'))
     model.add(tf.keras.layers.Activation('relu'))
-    model.add(MaxPooling1D(2, strides=2, name='block4_pool'))    
+   
     
     # Block 5
     model.add(Conv1D(512, 3, padding='same', name='block5_conv1',kernel_initializer = weight_init_method))
     model.add(Lambda(lambda x: K.l2_normalize(x,axis=1)))
     model.add(BatchNormalization(name='block5_batchnorm'))
     model.add(tf.keras.layers.Activation('relu'))
-    model.add(MaxPooling1D(2, strides=2, name='block5_pool')) 
+    model.add(AveragePooling1D(2, strides=2, name='block5_pool')) 
     
     model.add(Flatten(name='flatten'))
         
@@ -314,12 +314,11 @@ def cnn_best(input_length=2000, learning_rate=0.00001, classes=256, dense_units=
     model.add(BatchNormalization(name='block7_batchnorm'))
     model.add(tf.keras.layers.Activation('relu'))       
 
-    model.add(Dropout(0.5))
 
     
     model.add(Dense(classes, activation='softmax', name='predictions'))
 
-    optimizer = Adagrad(lr=learning_rate*10)
+    optimizer = RMSprop(lr=learning_rate)
     model.compile(loss=tf_median_probability_loss, optimizer=optimizer, metrics=['accuracy'])
     return model
 
