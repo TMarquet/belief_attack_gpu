9544f7e56a9bf9de5fb831824b4b28bcd1dd9f36
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-02-20 03:02:19 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -413,7 +413,7 @@ def train_variable_model(variable, X_profiling, Y_profiling, X_attack, Y_attack,
         sizes = [[20,40,80]]
         pooling = [[2]]
         filters = [3]
-        dense_layers = [2]
+        dense_layers = [3]
         dense_units = [4000]            
         for size in sizes:
             for pool in pooling:
