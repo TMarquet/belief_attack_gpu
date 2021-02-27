99e4b5617b6808e3e4dd9193a6c0ce6c412dce99
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-02-27 06:06:19 +0100

    test

--- belief_propagation_attack/realTraceHandler.py
+++ belief_propagation_attack/realTraceHandler.py
@@ -176,9 +176,9 @@ class RealTraceHandler:
                         # OLD: TODO FIX
                         # self.neural_network_dict[var_notrace] = load_sca_model('{}{}_mlp5_nodes200_window{}_epochs6000_batchsize200_sd100_traces200000_aug0.h5'.format(NEURAL_MODEL_FOLDER, var_notrace, tprange))
                         # NEW NEURAL NETWORKS 20/5/19                               p016_cnn_model1_window2000_epochs6_batchsize50_lr1e-05_sd100_traces240000_aug0_jitterNone.h5
-                        self.neural_network_dict[var_notrace] = load_sca_model('models/adagrad/{}_cnn_model1_window{}_epochs6_batchsize50_lr0.0001_sd100_traces240000_aug0_jitterNone_initwglorotu.h5'.format(var_notrace, tprange))
+                        #self.neural_network_dict[var_notrace] = load_sca_model('models/adagrad/{}_cnn_model1_window{}_epochs6_batchsize50_lr0.0001_sd100_traces240000_aug0_jitterNone_initwglorotu.h5'.format(var_notrace, tprange))
 
-                        #self.neural_network_dict[var_notrace] = load_sca_model('{}{}_mlp5_nodes100_window{}_epochs100_batchsize50_lr1e-05_sd100_traces190000_aug0_jitterNone_defaultloss_best.h5'.format(NEURAL_MODEL_FOLDER, var_notrace, tprange))
+                        self.neural_network_dict[var_notrace] = load_sca_model('{}{}_mlp5_nodes100_window{}_epochs100_batchsize50_lr1e-05_sd100_traces190000_aug0_jitterNone_defaultloss_best.h5'.format(NEURAL_MODEL_FOLDER, var_notrace, tprange))
                         
                         neural_network = self.neural_network_dict[var_notrace]
                    
@@ -196,7 +196,7 @@ class RealTraceHandler:
 
                 
                     #out_distribution_loaded = all_distribution[trace]   
-                    print('Value for neural network : ',normalise_array(out_distribution)[real_val])
+                    
                     #print('Value for saved proba : ',normalise_array(out_distribution_loaded)[real_val])
 
                 else:
