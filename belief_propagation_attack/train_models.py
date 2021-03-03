0d42b4f8cccbeede2c9775d40c023d1eb737d1ea
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-03-03 22:21:19 +0100

    test

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -415,10 +415,10 @@ def train_variable_model(variable, X_profiling, Y_profiling, X_attack, Y_attack,
     elif cnn:
         # TODO: Test New CNN!
         # cnn_best_model = cnn_best(input_length=input_length, learning_rate=learning_rate, classes=classes)
-        sizes = [[32,64,128]]
+        sizes = [[64,128,256,512,512]]
         pooling = [[2]]
-        filters = [3]
-        dense_layers = [3]
+        filters = [11]
+        dense_layers = [2]
         dense_units = [4000]            
         for size in sizes:
             for pool in pooling:
