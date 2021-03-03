2aa7e11b83310459d28853393970f5ac9845bf0c
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-03-03 16:40:01 +0100

    rzqr

--- belief_propagation_attack/train_models.py
+++ belief_propagation_attack/train_models.py
@@ -256,11 +256,11 @@ def cnn_best(input_length=2000, learning_rate=0.00001, filters = 3, classes=256,
     
     for i in range(len(size)):  
         if i == 0:
-            model.add(Conv1D(size[i], 1, padding='same', name='block{}_conv'.format(i+1),input_shape=input_shape))
+            model.add(Conv1D(size[i], 5, padding='same', name='block{}_conv'.format(i+1),input_shape=input_shape))
             model.add(Lambda(lambda x: K.l2_normalize(x,axis=1)))
             model.add(BatchNormalization())            
         else:
-            model.add(Conv1D(size[i], 1, padding='same', name='block{}_conv'.format(i+1)))
+            model.add(Conv1D(size[i], 5, padding='same', name='block{}_conv'.format(i+1)))
             model.add(Lambda(lambda x: K.l2_normalize(x,axis=1)))
             model.add(BatchNormalization())            
         model.add(Lambda(lambda x: K.l2_normalize(x,axis=1)))
