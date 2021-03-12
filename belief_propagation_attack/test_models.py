4f42c0074aff0be623b059c38320c5c09bb4c14f
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-03-13 00:34:50 +0100

    test

--- belief_propagation_attack/test_models.py
+++ belief_propagation_attack/test_models.py
@@ -439,7 +439,7 @@ if __name__ == "__main__":
     #     data.append(in_file)
     # data_np = np.array(data)
     model_tester = TestModels(jitter=JITTER, use_extra=(not RANDOM_KEY) and USE_EXTRA, no_print=not DEBUG, verbose=VERBOSE, histogram=HISTOGRAM)
-    variables_to_test = ['cm009','cm010','cm011','cm012']
+    variables_to_test = ['cm013','cm014','cm015','cm016']
     if TEST_ALL:
         # Clear statistics
         if SAVE:
