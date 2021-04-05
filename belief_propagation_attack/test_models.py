dcafe77c1731dc1f540f250f5284f22968763393
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-04-05 21:53:01 +0200

    tes

--- belief_propagation_attack/test_models.py
+++ belief_propagation_attack/test_models.py
@@ -447,7 +447,7 @@ if __name__ == "__main__":
     median_rank_out = []
     median_proba_out = []
     for i in range(1,17):
-        variables_to_test.append('xt0'+ ('0'+str(i) if i < 10 else '' + str(i)))
+        variables_to_test.append('cm0'+ ('0'+str(i) if i < 10 else '' + str(i)))
     print(variables_to_test)
     if TEST_ALL:
         # Clear statistics
