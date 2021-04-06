8337775955179889dec8e1969b202dee91a0faf3
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-04-06 17:31:58 +0200

    test

--- belief_propagation_attack/test_models.py
+++ belief_propagation_attack/test_models.py
@@ -448,7 +448,7 @@ if __name__ == "__main__":
     variables_to_test =[]
     median_rank_out = []
     median_proba_out = []
-    for i in range(1,5):
+    for i in range(5,9):
         variables_to_test.append('xt0'+ ('0'+str(i) if i < 10 else '' + str(i)))
     print(variables_to_test)
     if TEST_ALL:
