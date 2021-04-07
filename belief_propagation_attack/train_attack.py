56467a85f92855f494d21350f05f97937eb069b5
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-04-07 15:59:24 +0200

    test

--- belief_propagation_attack/train_attack.py
+++ belief_propagation_attack/train_attack.py
@@ -211,7 +211,9 @@ def train_variable_model(variable,mlp = False,cnn= False,epochs = 10,batch_size 
     for label in temp_label:
         data = [0]*256
         data[label] = 1
+        
         all_label.append(data)
+    print(temp_label[:10])
     training_data = np.array(all_data[:8000])
     training_label =np.array( all_label[:8000])
     validation_data = np.array(all_data[8000:])
