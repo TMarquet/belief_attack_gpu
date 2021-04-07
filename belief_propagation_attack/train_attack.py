7228162ec9bfd10b1987e3ffcf0e112acce417a6
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-04-07 16:12:49 +0200

    test

--- belief_propagation_attack/train_attack.py
+++ belief_propagation_attack/train_attack.py
@@ -194,7 +194,7 @@ def train_variable_model(variable,mlp = False,cnn= False,epochs = 10,batch_size 
 
     folder = 'output/s/'
     s = {}
-    real_values = np.load('{}{}.npy'.format(REALVALUES_FOLDER, var_name))
+    real_values = np.load('{}{}.npy'.format(REALVALUES_FOLDER, var_name))[var_number-1]
     all_data = []
     all_label = []
     temp_label = real_values[-10000:]
