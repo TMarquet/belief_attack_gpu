94f22238953c526184d0d104f9e4919de00959d2
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-04-12 15:03:22 +0200

    test

--- belief_propagation_attack/test_models.py
+++ belief_propagation_attack/test_models.py
@@ -474,7 +474,7 @@ if __name__ == "__main__":
                 for (m) in sorted(listdir(NEURAL_MODEL_FOLDER)):
                     var_name, var_number, _ = split_variable_name(var)
 
-                    if string_starts_with(m, 's001'):
+                    if string_starts_with(m, 's002'):
                         print 'Testing : ', m 
                         r,m = model_tester.check_model(NEURAL_MODEL_FOLDER + m, TEST_TRACES, template_attack=TEMPLATE_ATTACK, random_key=RANDOM_KEY, save=SAVE,ASCAD = ASCAD,save_proba=SAVE_PROBA,mlp=True)
                         median_rank_out.append(r)
