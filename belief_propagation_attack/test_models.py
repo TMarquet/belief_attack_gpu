5f9fdfacb5e6cee52c3b9193afce87eb7649a9ae
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-04-22 16:12:22 +0200

    test

--- belief_propagation_attack/test_models.py
+++ belief_propagation_attack/test_models.py
@@ -299,7 +299,7 @@ class TestModels:
                 else:
                     np.savetxt(OUTPUT_FOLDER + var_name + '/' + variable +'_all.csv', output_list, delimiter=',')
             else:
-                np.savetxt(OUTPUT_FOLDER + var_name + '/' + variable +'_rand_training_data.csv', output_list, delimiter=',')
+                np.savetxt(OUTPUT_FOLDER + var_name + '/' + variable +'_rand.csv', output_list, delimiter=',')
         if rank_list is not None:
 
             print "\n\nModel: {}".format(model_file)
