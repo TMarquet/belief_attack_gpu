ec16586549894ad0f4cbbf294be4050dacbea1e8
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-06-11 16:25:35 +0200

    test

--- belief_propagation_attack/factorGraphAES.pyx
+++ belief_propagation_attack/factorGraphAES.pyx
@@ -49,7 +49,7 @@ class FactorGraphAES:
         self.no_print = no_print
 
         if removed_nodes is None:
-            removed_nodes = []
+            removed_nodes = ['t','k','p']
         if left_out_nodes is None:
             left_out_nodes = []
 
