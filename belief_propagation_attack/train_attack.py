fdfff0de0a41fe26e3adbbd0a0c83160ba49c637
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-07-12 15:05:37 +0200

    tset

--- belief_propagation_attack/train_attack.py
+++ belief_propagation_attack/train_attack.py
@@ -212,7 +212,7 @@ def resave(number = [1]):
     for var , d in data.items():
         if not var == start_save:
             d.reshape((d.shape[0],d.shape[1],1))
-            end_data = np.concatenate(end_data,d,axis =1)
+            end_data = np.concatenate([end_data,d],axis =2)
     print(end_data.shape)
         
     
