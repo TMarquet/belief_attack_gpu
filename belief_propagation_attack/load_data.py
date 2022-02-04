055b097bfc0bcc4d89672e8f059c2190b6937620
TMarquet <72377479+TMarquet@users.noreply.github.com>
2022-02-04 20:15:28 +0100

    Update load_data.py

--- belief_propagation_attack/load_data.py
+++ belief_propagation_attack/load_data.py
@@ -8,9 +8,9 @@ Created on Fri Feb  4 08:11:54 2022
 from utility import *
 import pandas as pd
 from tqdm import tqdm
-traces = load_trace_data()
+traces = load_trace_data(filepath = TRACEDATA_EXTRA_FILEPATH)
 print(traces.shape)
-file = 'tracedata_pd.csv'
+file = 'tracedata_pd_extra.csv'
 df = pd.DataFrame(data=traces)
 print(df.shape)
 print(len(df))
