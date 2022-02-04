7583f3772ccd5da96c9914e12e3fe27ff4c002c9
TMarquet <72377479+TMarquet@users.noreply.github.com>
2022-02-04 08:16:25 +0100

    Update load_data.py

--- belief_propagation_attack/load_data.py
+++ belief_propagation_attack/load_data.py
@@ -6,6 +6,11 @@ Created on Fri Feb  4 08:11:54 2022
 """
 
 from utility import *
-
+import csv
 traces = load_trace_data()
 print(traces.shape)
+file = 'tracedata.csv'
+with open(file,'w',encoding='UTF8') as f:
+    write = csv.write(file)
+    for trace in traces:
+        writer.writerow(trace)
